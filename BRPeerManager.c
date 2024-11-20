f51d26c6515eddb2d2d67775ecd9c02ca2f1d3e8
vcexnet <36216930+vcexnet@users.noreply.github.com>
2024-11-20 12:14:18 +0800

    add:double fpRate
    add"double fpRate"

--- BRPeerManager.c
+++ BRPeerManager.c
@@ -1543,7 +1543,7 @@ static void _dummyThreadCleanup(void *info)
 
 // returns a newly allocated BRPeerManager struct that must be freed by calling BRPeerManagerFree()
 BRPeerManager *BRPeerManagerNew(const BRChainParams *params, BRWallet *wallet, uint32_t earliestKeyTime,
-                                BRMerkleBlock *blocks[], size_t blocksCount, const BRPeer peers[], size_t peersCount)
+                                BRMerkleBlock *blocks[], size_t blocksCount, const BRPeer peers[], size_t peersCount,double fpRate)
 {
     BRPeerManager *manager = calloc(1, sizeof(*manager));
     BRMerkleBlock orphan, *block = NULL;
